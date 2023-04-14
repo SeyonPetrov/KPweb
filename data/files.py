3b289c9ef1e824f5375a84694dab99e938998d61
SAYMON <urta.1732@mail.ru>
2023-04-14 22:27:04 +0500

    сделал бд, в любой момент ее можно расширить или по-другому изменить

---
+++ data/files.py
@@ -0,0 +1,18 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from data.db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Files(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'file'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, primary_key=True)
+    owner = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('user.id'))
+    type_f = sqlalchemy.Column(sqlalchemy.String, default='PDF')
+    url_address = sqlalchemy.Column(sqlalchemy.TEXT, nullable=False)
+    user = orm.relationship('User')
+
+    def __repr__(self):
+        return f'<Job> {self.job}'
