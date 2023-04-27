aa4182cb84099ecf896332c92f09aae074142e3b
SeyonPetrov <118447335+SeyonPetrov@users.noreply.github.com>
2023-04-28 00:01:34 +0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from data import db_session
-from data.users import User
-
-db_session.global_init('db/owners12.db')
-sess = db_session.create_session()
-
-user = sess.query(User).all()
-for x in user:
-    print(x.name)
